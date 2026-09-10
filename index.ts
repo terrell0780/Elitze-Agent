14edaa361838c6a02cc3fb17ad0adaa951306182
terrell Hall <terrell0780@gmail.com>
2026-09-10 08:52:15 -0700

    Export workflow and capability APIs

--- index.ts
+++ index.ts
@@ -4,3 +4,5 @@ export * from './router.js';
 export * from './memory.js';
 export * from './judge.js';
 export * from './agent.js';
+export * from './workflow.js';
+export * from './capabilities.js';
